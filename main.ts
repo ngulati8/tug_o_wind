33ab2199e1c6a7a0ff61b6e8a10ea14a32ffb87a
ngulati8 <131073597+ngulati8@users.noreply.github.com>
2024-03-28 14:55:25 -0400

    Quadrature Encoder rotation reading clockwise and anticlockwise added

--- main.ts
+++ main.ts
@@ -1,1 +1,30 @@
-
+RotaryEncoder.onRotateEvent(RotationDirection.Left, function () {
+    OLED12864_I2C.showNumber(
+    2,
+    2,
+    count2,
+    1
+    )
+    count2 += 1
+})
+RotaryEncoder.onRotateEvent(RotationDirection.Right, function () {
+    OLED12864_I2C.showNumber(
+    2,
+    2,
+    count2,
+    1
+    )
+    count2 += -1
+})
+let count2 = 0
+OLED12864_I2C.init(60)
+OLED12864_I2C.on()
+OLED12864_I2C.showString(
+2,
+2,
+"Hello!",
+1
+)
+led.enable(false)
+RotaryEncoder.init(DigitalPin.P5, DigitalPin.P6, DigitalPin.P7)
+OLED12864_I2C.clear()
